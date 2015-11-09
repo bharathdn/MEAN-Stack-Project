5243cb566ff910b705aaec8770024c820b8b811f
Bharath D N <bharathmh@gmail.com>
2015-11-09 00:57:33 -0500

    UI complete with navigation

--- public/Project/Client/config.js
+++ public/Project/Client/config.js
@@ -3,35 +3,38 @@
         .module("BukReviewApp",["ngRoute"])
         .config(function($routeProvider) {
             $routeProvider
-            .when("/",{
-                templateUrl: "home/home.view.html"
-            })
-            .when("/home",{
-                templateUrl: "home/home.view.html"
-            })
-            .when("/profile",{
-                templateUrl: "profile/profile.view.html"
-            })
-            .when("/register",{
-                templateUrl: "register/register.view.html"
-            })
-            .when("/search",{
-                templateUrl: "search/search.view.html"
-            })
-            .when("/search_result",{
-                templateUrl: "search/search_result.view.html"
-            })
-            .when("/favorites",{
-                templateUrl: "favorites/favorites-reviews.view.html"
-            })
-            .when("/favorite_books",{
-                templateUrl: "favorites/favorites-favorite.view.html"
-            })
-            .when("/login",{
-                templateUrl: "login/login.view.html"
-            })
-            .otherwise("/",
-                {redirectTo: "/"
-            });
+                .when("/",{
+                    templateUrl: "home/home.view.html"
+                })
+                .when("/home",{
+                    templateUrl: "home/home.view.html"
+                })
+                .when("/profile",{
+                    templateUrl: "profile/profile.view.html"
+                })
+                .when("/profile_friends",{
+                    templateUrl: "profile/profile_friends.view.html"
+                })
+                .when("/register",{
+                    templateUrl: "register/register.view.html"
+                })
+                .when("/search",{
+                    templateUrl: "search/search.view.html"
+                })
+                .when("/search_result",{
+                    templateUrl: "search/search_result.view.html"
+                })
+                .when("/favorites",{
+                    templateUrl: "favorites/favorites-reviews.view.html"
+                })
+                .when("/favorite_books",{
+                    templateUrl: "favorites/favorites-favorite.view.html"
+                })
+                .when("/login",{
+                    templateUrl: "login/login.view.html"
+                })
+                .otherwise("/",
+                    {redirectTo: "/"
+                });
         });
 })();
