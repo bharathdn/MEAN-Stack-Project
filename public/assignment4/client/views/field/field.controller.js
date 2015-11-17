3d499cbfd06ccf095356c321b2450d9d7b535e72
Bharath D N <bharathmh@gmail.com>
2015-11-17 18:13:36 -0500

    added all field types to controller

--- public/assignment4/client/views/field/field.controller.js
+++ public/assignment4/client/views/field/field.controller.js
@@ -19,30 +19,60 @@
             switch(fieldType)
             {
                 case "singlelinetext":
-                        field.label = "TextField";
-                        field.type = "TEXT";
-                        field.placeholder = "Text Field";
-                        break;
-                case
-                    "date":
-
+                    field = {"id": null,
+                        "label": "New Text Field",
+                        "type": "TEXT",
+                        "placeholder": "New Field"};
                     break;
-                case
-                    "dropdown":
 
+                case "paragraphtext":
+                    field = {"id": null,
+                        "label": "New Text Field",
+                        "type": "TEXTAREA",
+                        "placeholder": "New Field"};
                     break;
-                case
-                    "checkboxes":
 
+                case "date":
+                    field = {"id": null,
+                        "label": "New Date Field",
+                        "type": "DATE"};
                     break;
-                case
-                    "radiobuttons":
 
+                case "dropdown":
+                    field= {"id": null,
+                        "label": "New Dropdown",
+                        "type": "OPTIONS",
+                        "options": [
+                            {"label": "Option 1", "value": "OPTION_1"},
+                            {"label": "Option 2", "value": "OPTION_2"},
+                            {"label": "Option 3", "value": "OPTION_3"}]
+                    };
                     break;
-                case
-                    "paragraphtext":
 
+                case "checkboxes":
+                    field = {"id": null,
+                        "label": "New Checkboxes",
+                        "type": "CHECKBOXES",
+                        "options": [
+                            {"label": "Option A", "value": "OPTION_A"},
+                            {"label": "Option B", "value": "OPTION_B"},
+                            {"label": "Option C", "value": "OPTION_C"}
+                    ]};
                     break;
+
+                case "radiobuttons":
+                    field = {"id": null,
+                        "label": "New Radio Buttons",
+                        "type": "RADIOS",
+                        "options": [
+                        {"label": "Option X", "value": "OPTION_X"},
+                        {"label": "Option Y", "value": "OPTION_Y"},
+                        {"label": "Option Z", "value": "OPTION_Z"}
+                    ]};
+                    break;
+
+                default:
+                    console.log("Err!! cant find field: "+fieldType);
             }
 
             console.log(field);
