4633e5dd9c6df9ff78594dd845ceffae1026a368
Bharath D N <bharathmh@gmail.com>
2015-11-21 12:57:14 -0500

    Rhcloud url does not work,removed Mongo server config

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var multer = require('multer');
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/cs5610');
+//mongoose.connect('mongodb://localhost/cs5610');
 
 var app = express();
 //console.log(mongoose);
@@ -13,13 +13,13 @@ app.use(bodyParser.urlencoded({ extended: true })); //parse application/x-www-fo
 app.use(multer());//parse multipart/form-data
 
 //mongoose.connect('mongodb://localhost/cs5610');
-var db = mongoose.connection;
+//var db = mongoose.connection;
 //console.log(db);
 
 app.use(express.static(__dirname + '/public'));
 
-//require("./public/assignment4/server/app.js")(app);
-require("./public/assignment5/server/app.js")(app,db,mongoose);
+require("./public/assignment4/server/app.js")(app);
+//require("./public/assignment5/server/app.js")(app,db,mongoose);
 
 
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
