c08b98f78297fa0884d82f3841a5cd030f80a74b
Bharath D N <bharathmh@gmail.com>
2015-11-23 00:30:47 -0500

    Project: added google books api and tested

--- public/Project/Client/views/home/home.controller.js
+++ public/Project/Client/views/home/home.controller.js
@@ -4,18 +4,25 @@
         .controller("HomeController",HomeController);
 
 
-    function HomeController($location, $rootScope){
+    function HomeController($location, $rootScope,SearchService){
 
         var model = this;
 
         model.searchQuery = searchQuery;
 
-        console.log("Hello from controller");
+        //console.log("Hello from controller");
 
 
         function searchQuery(searchQueryString){
             //console.log("func called");
-            console.log(searchQueryString);
+            //console.log(searchQueryString);
+            var res = SearchService.searchGoogleBook(searchQueryString);
+                /*.then(function (searchResult) {
+                    console.log("reply from Searchservice");
+                    console.log(searchResult);
+                });*/
+            console.log("reply from Searchservice");
+            console.log(res);
         }
 
 
