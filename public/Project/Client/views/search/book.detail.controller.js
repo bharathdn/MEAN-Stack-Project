5fbd041e190a9be7f79969e81529fba23677b88e
Bharath D N <bharathmh@gmail.com>
2015-12-04 17:51:16 -0500

    writing reviews from Book Details page added

--- public/Project/Client/views/search/book.detail.controller.js
+++ public/Project/Client/views/search/book.detail.controller.js
@@ -8,10 +8,19 @@
 
         var model = this;
 
+
         //model.searchQuery = searchQuery;
-        model.addFav = addFav;
-        model.isLogin = isLogin;
+        //model.addFav = addFav;
+        model.isLogin           = isLogin;
+        model.submitReview      = submitReview;
+
+
+        function submitReview(userReview){
+            console.log("the Submitted Review: \n"+userReview);
+        }
+
         model.book =JSON.parse($window.sessionStorage.getItem("currentBook"));
+        //console.log(model.book);
 
         function addFav(book){
             console.log("You marked the book as favorite :"+ book.volumeInfo.title);
