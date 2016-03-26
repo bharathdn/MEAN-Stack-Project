26bc0a08fd348f3be0530933d1082e599760cd9d
Bharath D N <bharathmh@gmail.com>
2016-03-26 16:38:35 -0400

    disable register button when clicked and results are awaited

--- public/Project/Client/views/register/register.controller.js
+++ public/Project/Client/views/register/register.controller.js
@@ -8,6 +8,7 @@
     function RegisterController($location,ClientUserService,$rootScope) {
         //$scope.$location = $location;
         var model = this;
+        //model.submitDisabled = false;
         model.registerNewUser = registerNewUser;
         //model.message = "Hi";
 
@@ -15,14 +16,18 @@
         //TODO  Check if email already exists
         //console.log($rootScope.user);
         console.log("Register Controller!!!!!");
+
+
         function registerNewUser(user) {
-            console.log("Register called with following details");
-            console.log(user);
+            model.submitDisabled = true;
+            //console.log("Register called with following details");
+            //console.log(user);
             var registrationPossible = true;
             var userResult;
             if(!angular.isObject(user)) {
                 model.message = "All fields are mandatory!";
                 registrationPossible = false;
+                model.submitDisabled = false;
                 return;
             }
 
@@ -69,6 +74,9 @@
                         if(retuser != null) {
                             console.log(retuser.user);
                             registerCallback(retuser.user);
+                        }
+                        else {
+                            model.submitDisabled = false;
                         }
                     });
             }
