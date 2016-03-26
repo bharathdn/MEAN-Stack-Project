5f4053f445c5319c5d4549e43172f1b11cf04312
Bharath D N <bharathmh@gmail.com>
2016-03-26 16:58:41 -0400

    disable register button when clicked and results are awaited mod7

--- public/Project/Client/views/register/register.controller.js
+++ public/Project/Client/views/register/register.controller.js
@@ -62,6 +62,7 @@
                         console.log("Checking if User already exists");
                         console.log(userResult);
                         if (userResult) {
+                            console.log("username already present,returning");
                             registrationPossible = false;
                             model.message = "Username already exists, please choose a different username";
                             model.submitDisabled = false;
